e8ab590939fe28382613dd4971f27ba417977de4
Denis Korablev <35533038+DenKorablev@users.noreply.github.com>
2018-06-28 08:39:45 +0300

    Homework Obucharium

--- Lesson5/src/app/fake-contacts.ts
+++ Lesson5/src/app/fake-contacts.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Person } from './person';
 
 export const Contacts: Person[] = 
@@ -6,13 +5,4 @@ export const Contacts: Person[] =
         {id: 1, name:'Dima', number:'89207779988', town:'Moscow'},
         {id: 2, name:'Masha', number:'89153241122', town:'Pskov'},
         {id: 3, name:'Oleg', number:'89127779388', town:'St.Peterburg'},
-=======
-import { Person } from './person';
-
-export const Contacts: Person[] = 
-    [
-        {id: 1, name:'Dima', number:'89207779988', town:'Moscow'},
-        {id: 2, name:'Masha', number:'89153241122', town:'Pskov'},
-        {id: 3, name:'Oleg', number:'89127779388', town:'St.Peterburg'},
->>>>>>> 7174be6740652492cd0ea74f4ebe68e77107730f
     ];
