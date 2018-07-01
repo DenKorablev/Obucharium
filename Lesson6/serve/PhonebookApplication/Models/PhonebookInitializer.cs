d9edc7e46857f1111077a34f94b684c324a6c823
Denis Korablev <35533038+DenKorablev@users.noreply.github.com>
2018-07-01 23:36:14 +0300

    Lesson6

---
+++ Lesson6/serve/PhonebookApplication/Models/PhonebookInitializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PhonebookApplication.Models
+{
+
+    public class PhonebookInitializer : DropCreateDatabaseIfModelChanges<PhonebookDB>
+    {
+        public readonly string[] names =
+        {
+            "Jeff",
+            "Bill",
+            "Bernard",
+            "Mark",
+            "Carlos",
+            "David"
+        };
+        protected override void Seed(PhonebookDB context)
+        {
+            foreach (var name in names)
+            {
+                context.Contacts.Add(new Contact
+                {
+                    Name = name,
+                    Phone = Math.Abs((int)name.GetTypeCode()).ToString().Substring(0, 9).PadRight(6, '0')
+                });
+            }
+            base.Seed(context);
+        }
+    }
+}
